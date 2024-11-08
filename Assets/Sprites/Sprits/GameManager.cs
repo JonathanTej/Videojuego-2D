bef28e9a3e9d5a28dc6a36b170b6c77903104807
Jonathan Jiménez <tj0027032023@unab.edu.sv>
2024-11-08 11:19:37 -0600

    Agregar comentarios al código

--- Assets/Sprites/Sprits/GameManager.cs
+++ Assets/Sprites/Sprits/GameManager.cs
@@ -4,76 +4,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+/// Clase principal que gestiona la lógica del juego
+/// Controla el estado del juego, los obstáculos y el movimiento del escenario
 public class GameManager : MonoBehaviour
 {
-    public GameObject Menú;
-    public GameObject GameOver;
-    public float velocidad = 2;
-    public GameObject Columna;
-    public List<GameObject> columnas;
-    public List<GameObject> Obstaculos;
-    public GameObject Piedra;
-    public GameObject PiedraPequeña;
-    public bool gameOver = false;
-    public bool StartGame = false;
-    public Renderer fondo;
+    public GameObject Menú;//Menú de inicio
+    public GameObject GameOver;//Pantalla de Game Over
+    public float velocidad = 2; //Velocidad de movimiento del escenario
+    public GameObject Columna; //Prefab de la columna
+    public List<GameObject> columnas; //Lista de columnas
+    public List<GameObject> Obstaculos; //Lista de obstáculos
+    public GameObject Piedra;//Prefab de la piedra
+    public GameObject PiedraPequeña;//Prefab de la piedra pequeña
+    public bool gameOver = false;//Variable que indica si el juego ha terminado
+    public bool StartGame = false;//Variable que indica si el juego ha comenzado
+    public Renderer fondo;//Fondo del escenario
     // Start is called before the first frame update
     void Start()
         //crear mapa
     {
-        for(int i=0; i<21; i++ )
+        for(int i=0; i<21; i++) //Crear columnas
         {
-         columnas.Add(Instantiate(Columna, new Vector2(-10 + i, -3), Quaternion.identity));
+         columnas.Add(Instantiate(Columna, new Vector2(-10 + i, -3), Quaternion.identity)); //Instanciar columna
         }
         //Crear piedras
 
-        Obstaculos.Add(Instantiate(Piedra, new Vector2(15,-2), Quaternion.identity));
-        Obstaculos.Add(Instantiate(PiedraPequeña, new Vector2(20, -2), Quaternion.identity));
+        Obstaculos.Add(Instantiate(Piedra, new Vector2(15,-2), Quaternion.identity)); //Instanciar piedra
+        Obstaculos.Add(Instantiate(PiedraPequeña, new Vector2(20, -2), Quaternion.identity)); //Instanciar piedra pequeña
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(StartGame == false )
+        if(StartGame == false) //Si el juego no ha comenzado
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space)) //Si se presiona la tecla espacio
             {
-                StartGame = true;
+                StartGame = true; //Comenzar el juego
             }
         }
 
-        if (StartGame == true && gameOver == true)
+        if (StartGame == true && gameOver == true) //Si el juego ha comenzado y ha terminado
         {
-            GameOver.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Space))
+            GameOver.SetActive(true); //Mostrar pantalla de Game Over
+            if (Input.GetKeyDown(KeyCode.Space))  //Si se presiona la tecla espacio
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Recargar la escena
             }
         }
 
-        if (StartGame == true && gameOver == false)
+        if (StartGame == true && gameOver == false) //Si el juego ha comenzado y no ha terminado
         {
-            Menú.SetActive(false);
-            fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.025f, 0) * Time.deltaTime;
+            Menú.SetActive(false); 
+            fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.025f, 0) * Time.deltaTime; //Mover fondo
 
             //mover mapa
-            for (int i = 0; i < columnas.Count; i++)
+            for (int i = 0; i < columnas.Count; i++) //Mover columnas
             {
-                if (columnas[i].transform.position.x <= -10)
+                if (columnas[i].transform.position.x <= -10) //Si la columna sale de la pantalla
                 {
-                    columnas[i].transform.position = new Vector3(10, -3, 0);
+                    columnas[i].transform.position = new Vector3(10, -3, 0); //Reubicar la columna
                 }
-                columnas[i].transform.position = columnas[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad;
+                columnas[i].transform.position = columnas[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad; //Mover la columna
             }
             //spawnear piedras u obstaculos
-            for (int i = 0; i < Obstaculos.Count; i++)
+            for (int i = 0; i < Obstaculos.Count; i++) //Mover obstáculos
             {
-                if (Obstaculos[i].transform.position.x <= -10)
+                if (Obstaculos[i].transform.position.x <= -10) //Si el obstáculo sale de la pantalla
                 {
-                    float randomObs = Random.Range(11, 18);
-                    Obstaculos[i].transform.position = new Vector3(randomObs, -2, 0);
+                    float randomObs = Random.Range(11, 18); //Generar una posición aleatoria
+                    Obstaculos[i].transform.position = new Vector3(randomObs, -2, 0); //Reubicar el obstáculo
                 }
-                Obstaculos[i].transform.position = Obstaculos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad;
+                Obstaculos[i].transform.position = Obstaculos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad; //Mover el obstáculo
             }
         }
     }
--- Assets/Sprites/Sprits/Jugador.cs
+++ Assets/Sprites/Sprits/Jugador.cs
@@ -2,42 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jugador : MonoBehaviour
+public class Jugador : MonoBehaviour 
 {
-    public float fuerzaSalto;
-    public GameManager gameManager;
-
-    private Rigidbody2D rigidbody2D;
-
-
-
-    private Animator animator;
+    public float fuerzaSalto; //Fuerza de salto del jugador
+    public GameManager gameManager; // se llama a GameManager
+    private Rigidbody2D rigidbody2D; //Rigidbody del jugador
+    private Animator animator;//Animator del jugador
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); //Obtener el animator del jugador
+        rigidbody2D = GetComponent<Rigidbody2D>(); //Obtener el Rigidbody del jugador
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)) //Si se presiona la tecla espacio
         {
-            animator.SetBool("EstaSaltando", true);
-            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+            animator.SetBool("EstaSaltando", true); //Activar la animación de salto
+            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto)); //Aplicar fuerza de salto
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //Cuando el jugador colisiona con otro objeto
     {
-        if (collision.gameObject.tag == "Suelo")
+        if (collision.gameObject.tag == "Suelo") //Si colisiona con el suelo
         {
-            animator.SetBool("EstaSaltando", false);
+            animator.SetBool("EstaSaltando", false); //Desactivar la animación de salto
         }
-        if (collision.gameObject.tag == "Obstaculo")
+        if (collision.gameObject.tag == "Obstaculo") //Si colisiona con un obstáculo
         {
-            gameManager.gameOver = true;
+            gameManager.gameOver = true; //El juego ha terminado
         }
     }
 }
